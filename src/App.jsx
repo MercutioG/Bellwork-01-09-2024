98c69cf7d192fb3306ad6bbc37f8f6629acfdfcd
MercutioG <mgeant296@west-mec.org>
2024-01-09 18:12:51 -0700

    Completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import DynamicListContainer from './components/DynamicListContainer'
-import data from './data'
+import React from 'react';
+import DynamicListContainer from './components/DynamicListContainer';
+import data from './data';
 
 const App = () => {
   return (
-    <DynamicListContainer data={data} type={'ol'} orientation={'verticle'}/>
-  )
-}
+    <DynamicListContainer data={data} type="ol" orientation="vertical" />
+  );
+};
 
-export default App
+export default App;
--- src/components/DynamicListContainer.jsx
+++ src/components/DynamicListContainer.jsx
@@ -1,18 +1,39 @@
-const DynamicListContainer = (data, type, orientation) => {
-  console.log(orientation);
-  console.log(type);
-  console.log(data);
-  function checkParams(){
-    return `<${type} class='${orientation}'>
-      Hi
-    </${type}>`
+import React from 'react';
+
+const DynamicListContainer = ({ data, type, orientation }) => {
+  function checkParams() {
+    if (type === 'ul') {
+      return (
+        <ul className={orientation}>
+          {data.map((item, index) => (
+            <li className="gift-list" key={index}>
+              <p>Name: {item.Name}</p>
+              <p>Gift: {item.Gift}</p>
+              <p>Value: ${item.Value}</p>
+              <p>Weight: {item.Weight}lbs</p>
+              <p>Conductive: {String(item.Conductive).toUpperCase()}</p>
+            </li>
+          ))}
+        </ul>
+      );
+    } else if (type === 'ol') {
+      return (
+        <ol className={orientation}>
+          {data.map((item, index) => (
+            <li className="gift-list" key={index}>
+              <p>Name: {item.Name}</p>
+              <p>Gift: {item.Gift}</p>
+              <p>Value: ${item.Value}</p>
+              <p>Weight: {item.Weight}lbs</p>
+              <p>Conductive: {String(item.Conductive).toUpperCase()}</p>
+            </li>
+          ))}
+        </ol>
+      );
+    }
   }
 
-  return (
-    <>
-      {checkParams()}
-    </>
-  )
-}
+  return <div>{checkParams()}</div>; // wrap in a div or any other container
+};
 
-export default DynamicListContainer
+export default DynamicListContainer;
